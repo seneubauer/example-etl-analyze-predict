e0729bf0d6f59caee7107882bd0256e6ec627fdf
Scott Neubauer <scotteneubauer@gmail.com>
2022-11-18 15:36:30 -0600

    Commit progress

--- database_schema/schema1.sql
+++ database_schema/schema1.sql
@@ -1,3 +1,40 @@
+-- independent tables
+
+-- table of vendors for purchased components
+create table vendors (
+    uid integer unique not null,
+    name varchar(50) not null,
+    reputation decimal,
+    constraint uid_pk primary key (uid)
+);
+
+-- table of job orders for built components
+create table job_orders (
+    uid varchar(25) unique not null,
+    parameter0 decimal,
+    parameter1 integer,
+    parameter2 varchar(100),
+    primary key (uid)
+);
+
+create table purchase_orders (
+    uid varchar(25) unique not null,
+    primary key (uid)
+);
+
+create table unit_types (
+    uid integer unique not null,
+    name varchar(50) not null,
+    constraint uid_pk primary key (uid)
+)
+
+
+
+
+
+
+
+
 -- table of parts
 create table parts (
     uid integer unique not null,
